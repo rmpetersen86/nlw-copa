2c8e910896ddfa072bd0f5cddc134126bc86f5ec
rmpetersen86 <rafael.petersen86@gmail.com>
2022-11-01 18:12:52 -0300

    Adicionando mobile

---
+++ mobile/src/screens/SignIn.tsx
@@ -0,0 +1,11 @@
+import { Center, Text } from "native-base";
+
+export function SignIn() {
+    return (
+        <Center flex={1} bgColor="gray.600">      
+      <Text color="white" fontSize={32} fontFamily="heading">
+        SignIn
+      </Text>      
+    </Center>
+    )
+}
